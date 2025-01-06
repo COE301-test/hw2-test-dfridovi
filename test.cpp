b2ab16548c770f5b9469f560dc66dd6275dbbc62
dfridovi <dfk@utexas.edu>
2025-01-06 12:49:17 -0600

    make -f Makefile.txt

---
+++ test.cpp
@@ -0,0 +1,25 @@
+#include "hello.hpp"
+
+#include <iostream>
+
+#define TEST_EQUALS(x, y) if (x != y) return 1;
+#define TEST_CASE(f) if (f() != 0) return 1;
+
+int my_fancy_tests() {
+  Foo foo = Foo();
+  TEST_EQUALS(foo.Function(), EXPECTED_RESULT);
+
+  return 0;
+}
+
+int main(void) {
+  TEST_CASE(my_fancy_tests);
+
+  TEST_EQUALS(Factorial(0), -1);
+  TEST_EQUALS(Factorial(1), 1);
+  TEST_EQUALS(Factorial(2), 2);
+  TEST_EQUALS(Factorial(3), 6);
+  TEST_EQUALS(Factorial(10), 3628800);
+
+  return 0;
+}
